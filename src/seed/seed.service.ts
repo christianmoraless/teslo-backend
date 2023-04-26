1ac805f680c1e9b22446a69ddca279b9048a03a7
christianmoraless <christiandmoraless@gmail.com>
2023-04-26 14:45:49 -0400

    creating seed

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+// import { Repository } from 'typeorm';
+import { Product } from '../products/entities';
+import { ProductsService } from '../products/products.service';
+import { initialData } from './data/seed-data';
 
 @Injectable()
 export class SeedService {
+  constructor(private readonly productService: ProductsService) {}
+
   async runSeed() {
+    await this.insertNewProducts();
     return 'Runing Seed';
+  }
+
+  private async insertNewProducts() {
+    await this.productService.deleteAllProducts();
+    const products = initialData.products;
+    const insertPromise = [];
+    products.forEach((product) => {
+      insertPromise.push(this.productService.create(product));
+    });
+    Promise.all(insertPromise);
+    return true;
   }
 }
