a2a3c780afac0f7c232db0d5c246e281cb590349
christianmoraless <christiandmoraless@gmail.com>
2023-04-26 18:55:12 -0400

    aa

---
+++ src/files/files.controller.ts
@@ -0,0 +1,24 @@
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/file-filter';
+
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post('product')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+    }),
+  )
+  uploadProductImage(@UploadedFile() file: Express.Multer.File) {
+    return file;
+  }
+}
